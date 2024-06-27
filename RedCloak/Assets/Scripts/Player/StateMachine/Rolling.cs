db970a35c3d22587182e945323b65d5cbc630f4f
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-27 21:14:29 +0900

    *Rolling bug fixed

--- RedCloak/Assets/Scripts/Player/StateMachine/Rolling.cs
+++ RedCloak/Assets/Scripts/Player/StateMachine/Rolling.cs
@@ -1,14 +1,26 @@
 using AnimationImporter.PyxelEdit;
 using DG.Tweening;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Rolling : StateMachineBehaviour
 {
-
+    PlayerController controller;
+    float dir = 1f;
+    
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        PlayerController controller = animator.GetComponent<PlayerController>();
-        float dir = controller.GetComponent<SpriteRenderer>().flipX ? -1f : 1f;
-        controller.transform.DOMoveX(controller.transform.position.x + controller.maxSpeed * dir * 0.5f, 0.5f);
+        controller = animator.GetComponent<PlayerController>();
+        dir = controller.GetComponent<SpriteRenderer>().flipX ? -1f : 1f;
+        //controller.transform.DOMoveX(controller.transform.position.x + controller.maxSpeed * dir * 0.5f, 0.5f);
     }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (controller.Rolling)
+        {
+            controller.transform.position += new Vector3(dir, 0) * 1.2f * controller.maxSpeed * Time.deltaTime;
+        }
+    }
+
 }
