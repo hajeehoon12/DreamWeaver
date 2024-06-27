e3f3cdde30ccb10fede019bdd34d452d32bda081
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-27 13:48:55 +0900

    +Sound and effects

---
+++ RedCloak/Assets/Scripts/Util/Define.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public const string PLAYER_TAG = "Player";
+    public const string MONSTER_TAG = "Monster";
+    //public const string BULLET_TAG = "Bullet";
+    //public const string ITEM_TAG = "Item";
+    //public const string GROUND_Layer = "Ground";
+    //public const string BOSS_TAG = "Boss";
+    public const string FLOOR_TAG = "Floor";
+    
+    public const string MainScene = "TestScene-PDH";
+    public const string StartScene = "StartScene";
+}
