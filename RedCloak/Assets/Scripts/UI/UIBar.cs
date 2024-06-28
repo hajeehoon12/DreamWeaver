980d6ada9d6deb1c9236a318425b3b4935cc6211
EnBalor <enbalor4334@gmail.com>
2024-06-28 17:40:41 +0900

    +UIbar.cs
    UI 체력바 끝에 애니메이션 추가할 이미지 추가

--- RedCloak/Assets/Scripts/UI/UIBar.cs
+++ RedCloak/Assets/Scripts/UI/UIBar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,7 @@ public class UIBar : MonoBehaviour
     [SerializeField] private Slider playerHealthBar;
     [SerializeField] private Slider playerStaminaBar;
     [SerializeField] private Slider bossHealthBar;
+    public RectTransform endRect;
     
 
     public Monster monster;
@@ -15,12 +17,33 @@ public class UIBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        GetFillEndPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        bossHealthBar.value = monster.currentHealth / monster.maxHealth;
+        // 체력 시스템 추가 후 반영
+        //bossHealthBar.value = monster.currentHealth / monster.maxHealth;
+
+        GetFillEndPosition();
+    }
+
+    private void GetFillEndPosition()
+    {
+        if(endRect == null)
+        {
+            return;
+        }
+
+        float fillWidth = bossHealthBar.fillRect.rect.width;
+        float endPosition = (bossHealthBar.fillRect.anchoredPosition.x + fillWidth - 10f);
+
+        endRect.anchoredPosition = new Vector2(endPosition, endRect.anchoredPosition.y);
+    }
+
+    private void SetDamageEffectWidth(float width)
+    {
+
     }
 }
