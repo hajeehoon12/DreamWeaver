026b3e154681f5435935211bb91b9148d82ac713
EnBalor <enbalor4334@gmail.com>
2024-07-02 17:05:37 +0900

    *UI Fix
    UI 위치 및 이미지 수정

---
+++ RedCloak/Assets/Scripts/UI/CameraShake.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField] PlayerController playerController;
+    
+    public float duration = 0.5f;
+    public float strength = 0.3f;
+    public int vibrato = 10;
+    public float randomness = 90f;
+
+    private void LateUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            Shake();
+        }
+    }
+
+    public void Shake()
+    {
+        transform.DOShakePosition(duration, strength, vibrato, randomness);
+    }
+}
