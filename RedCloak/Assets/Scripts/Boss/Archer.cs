cb552c3a950788c10d2f16ef8d64797574a81a05
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-07-02 18:53:16 +0900

    *Animations

--- RedCloak/Assets/Scripts/Boss/Archer.cs
+++ RedCloak/Assets/Scripts/Boss/Archer.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class Archer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static readonly int doAttack = Animator.StringToHash("DoAttack");
+    private static readonly int doSpecialAttack = Animator.StringToHash("DoSpecialAttack");
+    private static readonly int isRunning = Animator.StringToHash("IsRunning");
+    private static readonly int isDead = Animator.StringToHash("IsDead");
+
+    private Animator animator;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+        Discrimination();
     }
+
+    void Discrimination()
+    { 
+        //Attack();
+        //WhenDie();
+        //SpecialAttack();
+    }
+
+
+    void Attack()
+    {
+        StartCoroutine(DoAttack());
+    }
+
+    IEnumerator DoAttack()
+    {
+        animator.SetTrigger(doAttack);
+        yield return new WaitForSeconds(1f);
+        Discrimination();
+    }
+
+    void SpecialAttack()
+    {
+        StartCoroutine(DoSpeciatAttack());
+    }
+
+    IEnumerator DoSpeciatAttack()
+    {
+        animator.SetTrigger(doSpecialAttack);
+        yield return new WaitForSeconds(1f);
+        Discrimination();
+    }
+
+    void WhenDie()
+    {
+        StartCoroutine(DoDie());
+    }
+
+    IEnumerator DoDie()
+    {
+        animator.SetBool(isDead, true);
+        yield return new WaitForSeconds(1f);
+        
+        // do die
+    }
+
+
+
+
+
+
 }
