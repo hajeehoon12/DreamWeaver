114fd1317e5a4d8d1f799fbe5e00dd22f354ee02
SnowScapes <siwon2113@gmail.com>
2024-07-05 10:53:06 +0900

    *Monster.cs
    일반몬스터 피격 및 사망 처리 스크립트로 수정

--- RedCloak/Assets/Scripts/Monster/Monster.cs
+++ RedCloak/Assets/Scripts/Monster/Monster.cs
@@ -1,22 +1,45 @@
+using BehaviorDesigner.Runtime;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    public bool GetHit = false;
+    
     public float maxHealth;
     public float currentHealth;
+
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Collider2D _collider;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private BehaviorTree _behavior;
 
     private void Start()
     {
         currentHealth = maxHealth;
     }
 
+    private void OnEnable()
+    {
+        _behavior.enabled = true;
+        _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        _collider.enabled = true;
+    }
 
     public void GetDamage(float damage)
     {
-        Debug.Log("InDamage");
-        if (currentHealth > damage) currentHealth -= damage;
-        else Destroy(gameObject);
+        if (currentHealth > damage)
+        {
+            GetHit = true;
+            _animator.SetTrigger("GetHit");
+            currentHealth -= damage;
+        }
+        else
+        {
+            _behavior.enabled = false;
+            _rigidbody.bodyType = RigidbodyType2D.Static;
+            _collider.enabled = false;
+            _animator.SetTrigger("Die");
+        }
     }
 }
