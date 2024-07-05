11f5b9288e1bb04aa17dd3115d2a899d5d048757
SnowScapes <siwon2113@gmail.com>
2024-07-05 21:02:11 +0900

    *Knockback.cs
    넉백 파워 변수 추가 (BT)

--- RedCloak/Assets/Scripts/Monster/KnockBack.cs
+++ RedCloak/Assets/Scripts/Monster/KnockBack.cs
@@ -6,12 +6,13 @@ public class KnockBack : Action
 {
     private float time;
     private Rigidbody2D _rigidbody;
+    public SharedFloat KnockbackPower;
     
 	public override void OnStart()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _rigidbody.velocity = Vector2.zero;
-        _rigidbody.AddForce(_rigidbody.mass * -5f * transform.right, ForceMode2D.Impulse);
+        _rigidbody.AddForce(_rigidbody.mass * -KnockbackPower.Value * transform.right, ForceMode2D.Impulse);
         time = 0f;
     }
 
