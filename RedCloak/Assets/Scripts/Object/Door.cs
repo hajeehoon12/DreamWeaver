949d20adca5cdef190c87b18e691311cd76b30e4
BY0808 <byahn.13@gmail.com>
2024-07-08 14:53:26 +0900

    *String Tag Update

--- RedCloak/Assets/Scripts/Object/Door.cs
+++ RedCloak/Assets/Scripts/Object/Door.cs
@@ -5,20 +5,32 @@ public class Door : MonoBehaviour
     public bool bossClear = false;
     [SerializeField] private GameObject door;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] private Archer archerBoss;
+
+    private void OpenDoor()
     {
-        if (collision.CompareTag("Player"))
-        {
-            door.SetActive(true);
-        }
+        //if (archerBoss.isBossDie)
+        //{
+        //    AudioManager.instance.PlaySFX("BoxOpen", 1f);
+        //    door.SetActive(false);
+        //}
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            door.SetActive(false);
-            bossClear = true;
-        }
-    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag(Define.PLAYER_TAG))
+    //    {
+    //        door.SetActive(true);
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag(Define.PLAYER_TAG))
+    //    {
+    //        door.SetActive(false);
+    //        bossClear = true;
+    //    }
+    //}
 }
