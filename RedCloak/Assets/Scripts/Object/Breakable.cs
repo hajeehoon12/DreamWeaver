00a0929655c878f794a0651869cf41110ad8ae2e
BY0808 <byahn.13@gmail.com>
2024-07-09 17:51:35 +0900

    *Breakable.cs Tag Update

--- RedCloak/Assets/Scripts/Object/Breakable.cs
+++ RedCloak/Assets/Scripts/Object/Breakable.cs
@@ -4,8 +4,9 @@ public class Breakable : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag(Define.PLAYER))
         {
+            AudioManager.instance.PlaySFX("success", 0.2f);
             gameObject.SetActive(false);
         }
     }
