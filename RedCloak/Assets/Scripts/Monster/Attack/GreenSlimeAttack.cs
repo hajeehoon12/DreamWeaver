951e8eb89a13573b7d219973e1a56b280e92c50f
SnowScapes <siwon2113@gmail.com>
2024-07-10 20:28:58 +0900

    *GreenSlimeAttack
    슬라임 어택 이름 수정

---
+++ RedCloak/Assets/Scripts/Monster/Attack/GreenSlimeAttack.cs
@@ -0,0 +1,65 @@
+using BehaviorDesigner.Runtime;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenSlimeAttack : MonoBehaviour, IMobAttack
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private MonsterController _controller;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private Transform playerTransform;
+
+    private bool onGround = true;
+
+    private void OnEnable()
+    {
+        _controller.MobAttack = this;
+        playerTransform = CharacterManager.Instance.Player.transform;
+    }
+
+    public bool PerformAttack()
+    {
+        AudioManager.instance.PlaySFX("SlimeAttack", 0.2f);
+        float x = (playerTransform.position.x - transform.position.x) / 2;
+        Vector2 centerPos = new Vector2(x, 5);
+        if (onGround)
+            JumpForce(centerPos);
+        return true;
+    }
+
+    private void JumpForce(Vector2 maxHeightPos)
+    {
+        float v_y = Mathf.Sqrt(2 * _rigidbody.gravityScale * -Physics2D.gravity.y * maxHeightPos.y);
+        // 포물선 운동 법칙 적용
+        float v_x = maxHeightPos.x * v_y / (2 * maxHeightPos.y);
+
+        Vector2 force = _rigidbody.mass * (new Vector2(v_x, v_y) - _rigidbody.velocity);
+        _rigidbody.AddForce(force, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer(Define.FLOOR) && !onGround)
+        {
+            _animator.SetTrigger("Fall");
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer(Define.FLOOR))
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer(Define.FLOOR))
+        {
+            onGround = false;
+        }
+    }
+}
