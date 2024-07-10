9d94a0a6a544f05d21430d224b5a1477f1fbdf35
SnowScapes <siwon2113@gmail.com>
2024-07-10 20:29:49 +0900

    *MonsterDeadAnim.cs

--- RedCloak/Assets/Scripts/Monster/MonsterDeadAnim.cs
+++ RedCloak/Assets/Scripts/Monster/MonsterDeadAnim.cs
@@ -7,8 +7,8 @@ public class MonsterDeadAnim : StateMachineBehaviour
     private Monster _monster;
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.enabled = false;
         _monster = animator.GetComponent<Monster>();
         _monster.StartCoroutine(_monster.SpawnLight());
+        animator.enabled = false;
     }
 }
