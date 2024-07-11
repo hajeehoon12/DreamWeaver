bbc2e03899eba9b2c65b3ad8da7277b5a3d4d968
BY0808 <byahn.13@gmail.com>
2024-07-11 14:29:58 +0900

    *Portal.cs

--- RedCloak/Assets/Scripts/Object/Portal.cs
+++ RedCloak/Assets/Scripts/Object/Portal.cs
@@ -7,17 +7,19 @@ public class Portal : MonoBehaviour
     [SerializeField] private Transform portalDestination;
 
     public int whichStage = 0;
-
+    public bool isStartPortal = true;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(Define.PLAYER))
         {
-            
-            CameraManager.Instance.CallStage2CameraInfo();
-            AudioManager.instance.PlaySFX("Success", 0.5f);
-            collision.gameObject.transform.position = portalDestination.position;
+            if (isStartPortal)
+            {
+                CameraManager.Instance.CallStage2CameraInfo();
+                StartCoroutine(TeleportAfterFade(collision.gameObject));
+                isStartPortal = false;
+            }
 
 
             switch (whichStage)
@@ -36,5 +38,19 @@ public class Portal : MonoBehaviour
             }
 
         }
+    }
+
+    private IEnumerator TeleportAfterFade(GameObject player)
+    {
+        FadeManager.instance.FadeOut();
+        yield return new WaitForSeconds(1f);
+
+        player.transform.position = portalDestination.position;
+
+        FadeManager.instance.FadeIn();
+        AudioManager.instance.PlaySFX("Success", 0.2f);
+        yield return new WaitForSeconds(2f);
+
+        isStartPortal = true;
     }
 }
