c31ea3aae884ebfcd89cbd28b0c0676211973a34
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-07-15 19:52:20 +0900

    Revert "Merge branch 'brand_new_sub_main' into HJH"
    This reverts commit ec60da0a80b0ab8dbacc62e49f2431811adf3c14, reversing
    changes made to 6508bc3c0e48300ff5d49525770d85993874aa74.

--- RedCloak/Assets/Scripts/Monster/MonsterController.cs
+++ RedCloak/Assets/Scripts/Monster/MonsterController.cs
@@ -1,32 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class MonsterController : MonoBehaviour
 {
+    [SerializeField] private float invincibleTime;
+    
     private WaitForSeconds invincible;
     public IMobAttack MobAttack;
 
-    private bool collisionOff = false;
-    public bool OnAttack { get; set; } = false;
-
     private void Start()
     {
-        invincible = new WaitForSeconds(CharacterManager.Instance.Player.battle.healthChangeDelay);
+        invincible = new WaitForSeconds(invincibleTime);
     }
 
     public void Attack()
     {
         if (MobAttack != null)
         {
-            OnAttack = true;
-            if (MobAttack.PerformAttack() && !collisionOff)
+            if (MobAttack.PerformAttack())
             {
-                collisionOff = true;
-                //SetLayerCollisionMatrix(LayerMask.NameToLayer(Define.PLAYER),LayerMask.NameToLayer(Define.ENEMY),false);
-                Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer(Define.PLAYER),LayerMask.NameToLayer(Define.ENEMY), true);
+                SetLayerCollisionMatrix(gameObject.layer, LayerMask.NameToLayer(Define.PLAYER), false);
                 StartCoroutine(collisionDelay());
             }
         }
@@ -34,12 +29,10 @@ public class MonsterController : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (!collisionOff && (1 << other.gameObject.layer & 1 << LayerMask.NameToLayer(Define.PLAYER)) != 0)
+        if ((1 << other.gameObject.layer & 1 << LayerMask.NameToLayer(Define.PLAYER)) != 0)
         {
-            collisionOff = true;
             other.gameObject.GetComponent<PlayerBattle>().ChangeHealth(-1, transform.position);
-            Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer(Define.PLAYER),LayerMask.NameToLayer(Define.ENEMY), true);
-            //SetLayerCollisionMatrix(LayerMask.NameToLayer(Define.PLAYER),LayerMask.NameToLayer(Define.ENEMY),false);
+            SetLayerCollisionMatrix(gameObject.layer, other.gameObject.layer, false);
             StartCoroutine(collisionDelay());
         }
     }
@@ -56,8 +49,6 @@ public class MonsterController : MonoBehaviour
     private IEnumerator collisionDelay()
     {
         yield return invincible;
-        //SetLayerCollisionMatrix(LayerMask.NameToLayer(Define.PLAYER),LayerMask.NameToLayer(Define.ENEMY),true);
-        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer(Define.PLAYER),LayerMask.NameToLayer(Define.ENEMY), false);
-        collisionOff = false;
+        SetLayerCollisionMatrix(7,11,true);
     }
 }
