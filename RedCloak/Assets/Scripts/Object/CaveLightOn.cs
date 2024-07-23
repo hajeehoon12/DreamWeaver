5f68d3fc3fa6b106c026dd4b0f035e1b41678693
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-07-23 16:06:31 +0900

    *bgm of cave

--- RedCloak/Assets/Scripts/Object/CaveLightOn.cs
+++ RedCloak/Assets/Scripts/Object/CaveLightOn.cs
@@ -17,6 +17,7 @@ public class CaveLightOn : MonoBehaviour
         {
             TurnOnCaveLight();
             isCaveLightOn = true;
+            AudioManager.instance.StopBGM();
             AudioManager.instance.PlayBGM2("CaveDrop", 0.2f);
         }
         else
@@ -32,6 +33,7 @@ public class CaveLightOn : MonoBehaviour
         {
             TurnOnCaveLight();
             isCaveLightOn = true;
+            AudioManager.instance.StopBGM();
             AudioManager.instance.PlayBGM2("CaveDrop", 0.2f);
         }
     }
