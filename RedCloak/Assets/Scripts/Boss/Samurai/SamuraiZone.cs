0dc861deb97d0097ab78c82aee5bb63bdc33b61d
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-07-26 22:30:05 +0900

    +SamuraiZone

---
+++ RedCloak/Assets/Scripts/Boss/Samurai/SamuraiZone.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamuraiZone : MonoBehaviour
+{
+    public Samurai samurai;
+    public bool enterZone = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (samurai.isBossDie) return;
+        if (collision.gameObject.CompareTag("Player") && !enterZone)
+        {
+            samurai.CallSamurai();
+            enterZone = true;
+        }
+    }
+}
