1ad20b4312c02eeb51b4aa939562a1e682cb206d
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-08-12 11:12:56 +0900

    +save point and call die reactivate

--- RedCloak/Assets/Scripts/Object/SavePoint.cs
+++ RedCloak/Assets/Scripts/Object/SavePoint.cs
@@ -4,7 +4,7 @@ public class SavePoint : MonoBehaviour, IDamage
 {    private void ShowSavePopup()
     {
         //저장하기 메세지 보여주기
-        Debug.Log("저장하기");
+        //Debug.Log("저장하기");
 
         CharacterManager.Instance.SavePoint = this.transform.position;
         CameraManager.Instance.SaveStage();
