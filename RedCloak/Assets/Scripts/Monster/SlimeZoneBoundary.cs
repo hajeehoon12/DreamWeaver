0b3068ae38dfb91d29bda83473f047c8e4cf5639
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-08-12 12:42:36 +0900

    +OrangeSlime doorzone

---
+++ RedCloak/Assets/Scripts/Monster/SlimeZoneBoundary.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeZoneBoundary : MonoBehaviour
+{
+    Collider2D col;
+    public Door door;
+
+    float xPos = 0;
+
+    private void Awake()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer(Define.PLAYER))
+        { 
+            xPos = gameObject.transform.position.x;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer(Define.PLAYER))
+        {
+            if (collision.gameObject.transform.position.x < xPos)
+            {
+                col.isTrigger = false;
+                door.CloseDoor();
+            }
+        }
+    }
+
+
+}
