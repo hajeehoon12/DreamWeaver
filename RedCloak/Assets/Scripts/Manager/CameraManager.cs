543d3b69e179276495bfe0c89d0ff967b4fac71f
BY0808 <byahn.13@gmail.com>
2024-08-14 00:13:46 +0900

    *CameraManager.cs
    빌드 오류로 인한 using 문 삭제

--- RedCloak/Assets/Scripts/Manager/CameraManager.cs
+++ RedCloak/Assets/Scripts/Manager/CameraManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.Rendering.Universal;
-using UnityEditor.SceneManagement;
 
 
 public class CameraManager : MonoBehaviour
