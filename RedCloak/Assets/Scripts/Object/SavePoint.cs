82e65149f0a596eccaf7f9403768bd5dfa3dcfa9
SnowScapes <siwon2113@gmail.com>
2024-08-21 17:51:06 +0900

    *Remove Unused Variable
    사용되지 않는 변수 삭제

--- RedCloak/Assets/Scripts/Object/SavePoint.cs
+++ RedCloak/Assets/Scripts/Object/SavePoint.cs
@@ -6,14 +6,12 @@ public class SavePoint : MonoBehaviour, IDamage
         //저장하기 메세지 보여주기
         //Debug.Log("저장하기");
 
-        CharacterManager.Instance.SavePoint = this.transform.position;
         CameraManager.Instance.SaveStage();
         //CharacterManager.Instance.Player.controller.
         CharacterManager.Instance.Player.stats.playerHP = CharacterManager.Instance.Player.stats.playerMaxHP;
         CharacterManager.Instance.Player.battle.ChangeHealth(0);
         UIManager.Instance.uiBar.SetCurrentHP();
         CharacterManager.Instance.SaveInfo();
-        //CharacterManager.Instance.SaveStage = CameraManager.Instance.stageNum;
         SaveLoad.Save("MobData", MonsterDataManager.mobArray);
         SaveLoad.Save("DoorData", CharacterManager.Instance.doorData);
         SaveLoad.Save("RewardBoxData", RewardBoxDataManager.array);
